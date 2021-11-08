1786cda641623b450d0c0db87500879901cc9a30
audukehinde <auduomohkehinde@gmail.com>
2021-11-08 06:04:04 -0500

    Add hardcoded background color when button is clicked

--- setup/hex.js
+++ setup/hex.js
@@ -1,1 +1,10 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = ["green", "red", "blue", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+  const randomNumber = 1;
+  document.body.style.backgroundColor = hex[randomNumber];
+  color.textContent = hex[randomNumber];
+})
