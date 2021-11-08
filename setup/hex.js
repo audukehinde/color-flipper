c60f866d184ed4f15a25be2962cf65e9cb6dc154
audukehinde <auduomohkehinde@gmail.com>
2021-11-08 06:14:39 -0500

    Add background color on the fly to index.html

--- setup/hex.js
+++ setup/hex.js
@@ -1,10 +1,1 @@
-const hex = ["green", "red", "blue", "rgba(133,122,200)", "#f15025"];
-
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
-
-btn.addEventListener('click', function(){
-  const randomNumber = 1;
-  document.body.style.backgroundColor = hex[randomNumber];
-  color.textContent = hex[randomNumber];
-})
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
